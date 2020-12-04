1982a48539c161b8e90e8691dfb8645f7b4933e2
m6xcom <65496114+m6xcom@users.noreply.github.com>
2020-12-04 13:36:19 +0200

    Добавляет меню еды

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+
+import menu from './menu.json';
+
+import cardTemplate from './templates/food-card.hbs';
+
+const menuList = document.querySelector('.js-menu');
+
+const posts = cardTemplate(menu);
+
+menuList.insertAdjacentHTML('afterbegin', posts);
